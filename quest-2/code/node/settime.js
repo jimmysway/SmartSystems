07f1b328c55258d2898786f87624eaa27a4b9ac2
howeyour <jli3469@bu.edu>
2023-10-06 22:33:42 -0400

    implement writing to csv file

--- quest-2/code/node/settime.js
+++ quest-2/code/node/settime.js
@@ -3,6 +3,22 @@ const { DateTime } = require('luxon');
 
 const port = new SerialPort({ path: '/dev/cu.usbserial-0264FEBE', baudRate: 115200 });
 
+// add
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const fs = require('fs');
+const Papa = require('papaparse');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const {ReadlineParser} = require('@serialport/parser-readline');
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+const textFileStream = fs.createWriteStream('data.csv', { flags: 'a' });
+//
+
 let now = DateTime.now();
 let currentTime = now.toFormat('HH:mm:ss');
 
@@ -22,7 +38,19 @@ port.write(dataToSend, (err) => {
     }
 });
 
-function readFromPort() {
+port.on("open", () => {
+    textFileStream.write('Time,Step,Temp\n');
+    });
+    parser.on('data', data =>{
+        let content;
+        now = DateTime.now();
+        currentTime = now.toFormat('HH:mm:ss');
+        content = currentTime + "," + data + '\n';
+        console.log(data);
+        textFileStream.write(content);
+    });
+
+    function readFromPort() {
     port.once('data', (data) => {
         console.log(`Received: ${data.toString()}`);
     });
